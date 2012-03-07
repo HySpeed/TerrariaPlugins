2d61bedd9c874a3d001af7cba7890c5cf4213db0
_Jon <hyspeed_games@yahoo.com>
2012-03-06 20:12:33 -0500

    Ver 1.2.0.2 * Fixed exit on server shutdown * Enabled Remote Commands (! -> /)
    Ver 1.2.0.1
    * Added Commands.init() call to Initialize()

---
+++ TerrariaIRC/README.md
@@ -0,0 +1,13 @@
+## TerrariaIRC
+
+
+Ver 1.2.0.2
+* Fixed exit on server shutdown
+* Enabled Remote Commands (! -> /)
+
+Ver 1.2.0.1
+* Added Commands.init() call to Initialize()
+
+
+Ver 1.2.0.0
+* Created by DeathMax
--- TerrariaIRC/TerrariaIRC.cs
+++ TerrariaIRC/TerrariaIRC.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using Hooks;
@@ -13,7 +10,6 @@ using Meebey.SmartIrc4net;
 using TShockAPI;
 using Terraria;
 using ErrorEventArgs = Meebey.SmartIrc4net.ErrorEventArgs;
-using Group = TShockAPI.Group;
 
 namespace TerrariaIRC
 {
@@ -21,12 +17,6 @@ namespace TerrariaIRC
     public class TerrariaIRC : TerrariaPlugin
     {
         #region Plugin Properties
-
-        public TerrariaIRC(Main game) : base(game)
-        {
-            Order = 10;
-        }
-
         public override string Name
         {
             get { return "TerrariaIRC"; }
@@ -44,82 +34,79 @@ namespace TerrariaIRC
 
         public override Version Version
         {
-            get { return new Version(1, 2, 0, 0); }
+            get { return new Version(1, 2, 0, 2); }
         }
-
+        public TerrariaIRC(Main game) : base(game)
+        {
+            Order = 10;
+        }
         #endregion
 
         #region Plugin Vars
-
         public static IrcClient irc = new IrcClient();
-        private static readonly Settings settings = new Settings();
+        private static Settings settings = new Settings();
         public static string settingsfiles = Path.Combine(TShock.SavePath, "irc", "settings.txt");
         public static bool StayConnected = true;
-        public static Dictionary<string, Group> Users = new Dictionary<string, Group>();
-
         #endregion
 
         #region Plugin overrides
-
         public override void Initialize()
         {
-            ServerHooks.Chat += OnChat;
-            ServerHooks.Join += OnJoin;
+            ServerHooks.Chat  += OnChat;
+            ServerHooks.Join  += OnJoin;
             ServerHooks.Leave += OnLeave;
-            irc.Encoding = Encoding.ASCII;
+            irc.Encoding = System.Text.Encoding.ASCII;
             irc.SendDelay = 300;
             irc.ActiveChannelSyncing = true;
             irc.AutoRejoinOnKick = true;
-            irc.OnQueryMessage += OnQueryMessage;
+            //irc.OnQueryMessage += OnQueryMessage;
             irc.OnError += OnError;
             irc.OnChannelMessage += OnChannelMessage;
             irc.OnRawMessage += OnRawMessage;
-            irc.OnNickChange += OnNickChange;
             if (!settings.Load())
             {
                 Console.WriteLine("Settings failed to load, aborting IRC connection.");
                 return;
             }
-            new Thread(Connect).Start();
+            Commands.Init();
+            new Thread( Connect ).Start();
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (!disposing) return;
-            StayConnected = false;
-            if (irc.IsConnected)
-                irc.Disconnect();
-        }
+          if ( disposing )
+          {
 
+            StayConnected = false;
+            if ( irc.IsConnected )
+              irc.Disconnect();
+            ServerHooks.Chat  -= OnChat;
+            ServerHooks.Join  -= OnJoin;
+            ServerHooks.Leave -= OnLeave;
+            base.Dispose( disposing );
+
+          } // if
+        }
         #endregion
 
         #region IRC methods
-
+        // _Jon : not functional 03-03-12 -> moved to "OnChannelMessage"
         public static void OnQueryMessage(object sender, IrcEventArgs e)
         {
-            string message = e.Data.Message;
-            if (IsOpAllowed(e.Data.Nick))
+          var message = e.Data.Message;
+            if ( IsAllowed( e.Data.Nick ) )
             {
                 if (message.StartsWith("!"))
                 {
-                    var user = new IRCPlayer(e.Data.Nick) {Group = new SuperAdminGroup()};
-                    TShockAPI.Commands.HandleCommand(user, "/" + message.TrimStart('!'));
-                    foreach (string t in user.Output)
-                        irc.RfcPrivmsg(e.Data.Nick, t);
+                  Console.WriteLine( "~ ! : " + message );
+                  var user = new IRCPlayer( e.Data.Nick ) { Group = new SuperAdminGroup() };
+                  String conCommand = "/" + message.TrimStart( '!' );
+                  TShockAPI.Commands.HandleCommand( user, conCommand );
+                  foreach ( var t in user.Output )
+                  {
+                    irc.RfcPrivmsg( e.Data.Nick, t );
+                  } // for
                 }
-            }
-            else if (IsUserAllowed(e.Data.Nick))
-            {
-                if (message.StartsWith("!"))
-                {
-                    var user = new IRCPlayer(e.Data.Nick) {Group = new Group(Users[e.Data.Nick].Name)};
-                    TShockAPI.Commands.HandleCommand(user, "/" + message.TrimStart('!'));
-                    foreach (string t in user.Output)
-                        irc.RfcPrivmsg(e.Data.Nick, t);
-                }
-            }
-            else if (message.ToLower().StartsWith("login"))
-            {
             }
             else
                 irc.RfcPrivmsg(e.Data.Nick, "You are not allowed to perform that action.");
@@ -130,29 +117,38 @@ namespace TerrariaIRC
             Console.WriteLine("IRC Error: {0}", e.Data.RawMessage);
         }
 
-        private static void OnChannelMessage(object sender, IrcEventArgs e)
+        void OnChannelMessage(object sender, IrcEventArgs e)
         {
-            string message = e.Data.Message;
-            if (message.ToLower() == "!players")
+            var message = e.Data.Message;
+            if ( message.StartsWith( "!" ) )
             {
-                string reply = TShock.Players.Where(player => player != null).Where(player => player.RealPlayer).Aggregate("", (current, player) => current + (current == "" ? player.Name : ", " + player.Name));
-                irc.SendMessage(SendType.Message, settings["channel"], "Current Players: " + reply);
-            }
+              if ( message.ToLower() == "!players" )
+              {
+                var reply = TShock.Players.Where( player => player != null ).Where( player => player.RealPlayer ).Aggregate( "", ( current, player ) => current + (current == "" ? player.Name : ", " + player.Name) );
+                irc.SendMessage( SendType.Message, settings["channel"], "Current Players: " + reply );
+              } // if
+              else
+              {
+                var user = new IRCPlayer( e.Data.Nick ) { Group = new SuperAdminGroup() };
+                String conCommand = "/" + message.TrimStart( '!' );
+                Console.WriteLine( "~ !/: " + message );
+                TShockAPI.Commands.HandleCommand( user, conCommand );
+                foreach ( var t in user.Output )
+                {
+                  irc.RfcPrivmsg( e.Data.Nick, t );
+                } // for
+              } // else
+            } // if
             else
-                TShock.Utils.Broadcast(string.Format("(IRC)<{0}> {1}", e.Data.Nick,
-                    TShock.Utils.SanitizeString(Regex.Replace(message, (char) 3 + "[0-9]{1,2}(,[0-9]{1,2})?", String.Empty))), Color.Green);
+            {
+              TShock.Utils.Broadcast( string.Format( "(IRC)<{0}> {1}", e.Data.Nick,
+                  TShock.Utils.SanitizeString( Regex.Replace( message, (char) 3 + "[0-9]{1,2}(,[0-9]{1,2})?", String.Empty ) ) ), Color.Green );
+            } // else
         }
 
-        private static void OnRawMessage(object sender, IrcEventArgs e)
+        void OnRawMessage(object sender, IrcEventArgs e)
         {
             Debug.Write(e.Data.RawMessage);
-        }
-
-        private static void OnNickChange(object sender, NickChangeEventArgs e)
-        {
-            if (!Users.ContainsKey(e.OldNickname)) return;
-            Users.Add(e.NewNickname, Users[e.OldNickname]);
-            Users.Remove(e.OldNickname);
         }
 
         public static void Connect()
@@ -188,32 +184,27 @@ namespace TerrariaIRC
             }
         }
 
-        public static bool IsOpAllowed(string nick)
+        public static bool IsAllowed(string nick)
         {
             //Theres an issue with IsOp, if the ircd has anything higher like +a, IsOp will return false;
             if (bool.Parse(settings["allowop"]))
+            {
                 return (from user in (from DictionaryEntry channeluser in irc.GetChannel(settings["channel"]).Users select (ChannelUser) channeluser.Value) where user.Nick == nick select user.IsOp).FirstOrDefault();
+            }
             return false;
-        }
-
-        public static bool IsUserAllowed(string nick)
-        {
-            return Users.ContainsKey(nick);
         }
 
         public static bool CompareIrcUser(IrcUser user1, IrcUser user2)
         {
             return (user1.Host == user2.Host && user1.Ident == user2.Ident && user1.Realname == user2.Realname);
         }
-
         #endregion
 
         #region Plugin hooks
-
-        private void OnChat(messageBuffer msg, int player, string text, HandledEventArgs e)
+        void OnChat(messageBuffer msg, int player, string text, System.ComponentModel.HandledEventArgs e)
         {
             if (!irc.IsConnected) return;
-            TSPlayer tsplr = TShock.Players[msg.whoAmI];
+            var tsplr = TShock.Players[msg.whoAmI];
             if (tsplr == null)
                 return;
             if (!TShock.Utils.ValidString(text))
@@ -222,25 +213,24 @@ namespace TerrariaIRC
                 return;
             if (tsplr.mute)
                 return;
-            irc.SendMessage(SendType.Message, settings["channel"], string.Format("({0}){1}: {2}",
+            irc.SendMessage(SendType.Message, settings["channel"], string.Format("({0}){1}: {2}", 
                 tsplr.Group.Name, tsplr.Name, text));
         }
 
-        private void OnJoin(int player, HandledEventArgs e)
+        void OnJoin(int player, System.ComponentModel.HandledEventArgs e)
         {
             if (!irc.IsConnected) return;
             if (e.Handled) return;
-            irc.SendMessage(SendType.Message, settings["channel"], string.Format("{0} joined the server.",
+            irc.SendMessage(SendType.Message, settings["channel"], string.Format("{0} joined the server.", 
                 Main.player[player].name));
         }
 
-        private void OnLeave(int player)
+        void OnLeave(int player)
         {
             if (!irc.IsConnected) return;
             irc.SendMessage(SendType.Message, settings["channel"], string.Format("{0} left the server.",
                 Main.player[player].name));
         }
-
         #endregion
     }
 }
